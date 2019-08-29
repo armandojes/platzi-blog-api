736cfc54caef6ec757636cb2e48530c572c7de0e
unknown <armandodejesus678@gmail.com>
2019-08-29 16:57:46 -0500

    fix rote popular posts

--- routes.php
+++ routes.php
@@ -5,7 +5,7 @@ $Router = new Router();
 $Router->get('/test', 'test_controller');
 $Router->get('/update', 'update_controller');
 $Router->get('/posts/{page}', 'posts_controller');
-$Router->get('/posts/pupular/{page}', 'posts_populars_controller');
+$Router->get('/posts/popular/{page}', 'posts_populars_controller');
 $Router->get('/postprimary', 'postsprimary_controller');
 $Router->get('/search/{query}/{page}', 'search_controller');
 $Router->get('/post/{url}', 'post_controller');
