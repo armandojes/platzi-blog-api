59e727f33c7eee8ed5df646dff2d97cef78b355e
unknown <armandodejesus678@gmail.com>
2019-09-03 12:11:47 -0500

    verificar la existencia de comentarios y retornar string vacio

--- core/controllers/comments_controller.php
+++ core/controllers/comments_controller.php
@@ -15,17 +15,3 @@ class comments_controller extends controller {
 
   }
 }
-
-
-// $Post = new Post();
-//
-// $post = $Post->get_post_primary_full();
-// $comments = $this->params->url === $post['url']
-//  ? $Post->get_comments_primary()
-//  : $Post->get_comments($this->params->url);
-//
-// $this->response([
-//   'error' => $comments === false ? true : false,
-//   'error_message' => $comments === false ? 'error 404 comment not found' : '',
-//   'data' => $comments === false ? '' : $comments['comments'],
-// ]);
